50c5a5330490ed7363c71ede8f9e4a39a1263c4d
Greg Little <glittle@gmail.com>
2015-01-16 02:53:11 -0800

    help prevent evil, maybe

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 get_github_access_token = function (scope, cb) {
 	$.get('https://github-access-token-getter.herokuapp.com/client_id', function (client_id) {
 		var state = _.randomString(20)
-		window.get_github_access_token_callback = function (access_token) {
+		window.get_github_access_token_callback = function (access_token, state2) {
+			if (state != state2) throw 'evil is amok'
 			cb(access_token)
 		}
 		var w = window.open('https://github.com/login/oauth/authorize?client_id=' + client_id + '&scope=' + scope + '&state=' + state, 'github','height=500,width=1024')
