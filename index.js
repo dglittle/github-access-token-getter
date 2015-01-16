def67e6b7c1f4ab2026c2d7ce0000a951ec52790
Greg Little <glittle@gmail.com>
2015-01-16 04:14:05 -0800

    removes event listener, so they don't accumulate, though I don't know for sure that they would, but it seems like they would

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ get_github_access_token = function (scope, cb) {
 
 		var func = function (event) {
 			if (event.origin != 'https://github-access-token-getter.herokuapp.com') return
+			removeEventListener("message", func, false)
 			var o = _.unJson(event.data)
 			if (state != o.state) throw 'evil is amok'
 			cb(o.access_token)
 		}
-		if (window.addEventListener)
-			addEventListener("message", func, false)
-		else
-			attachEvent("onmessage", func)
+		addEventListener("message", func, false)
 
 		var w = window.open('https://github.com/login/oauth/authorize?client_id=' + client_id + '&scope=' + scope + '&state=' + state, 'github','height=500,width=1024')
 		if (window.focus) w.focus()
